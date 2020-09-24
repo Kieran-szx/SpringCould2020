5b1f0f8d53321bd3fa474f9238353b9782908840
shizexun <szx6577588@163.com>
2020-09-24 16:37:20 +0800

    gateway的perdicate功能

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author Shizx
+ * @date 2020/9/21 17:28
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
